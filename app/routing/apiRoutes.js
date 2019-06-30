e719323788600af272e6eab71a91dec1aa4579f2
Chloe Zhou <chloezhouny@gmail.com>
2019-06-29 23:13:30 -0700

    Add api survey get and post requests, friend.js data

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,37 @@
+// ===============================================================================
+// LOAD DATA
+// linking rout to a series of "data" sources.
+// ===============================================================================
+
+var friendData = require("../data/friends");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/survey", function(req, res) {
+    res.json(friendData);
+  });
+
+
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/survey", function(req, res) {
+      var newUser = req.body;
+      res.json(true);
+
+  });
+
+};
