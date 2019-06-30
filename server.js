e1cea857dffe291df84e5d47b0a1402277a15a7f
Chloe Zhou <chloezhouny@gmail.com>
2019-06-29 22:59:27 -0700

    add html get requests in htmlRountes.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require("express");
 
 
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
 var app = express();
 
 var PORT = process.env.PORT || 8080;
