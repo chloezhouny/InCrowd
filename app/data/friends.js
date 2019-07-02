259960000a345218d2e1afe3a2f0b089d8d2357f
Chloe Zhou <chloezhouny@gmail.com>
2019-07-01 21:06:57 -0700

    Matched user data can be sent back to client side

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,4 @@
 
-
-
 var friendData = 
 [
   {
@@ -18,7 +16,56 @@ var friendData =
       4,
       1
     ]
-  } 
+  },
+  {
+    name: "Ahmed",
+    songLink: "https://www.youtube.com/watch?v=zqv3YesdtRU",
+    scores: [
+      2,
+      1,
+      2,
+      4,
+      3,
+      1,
+      2,
+      5,
+      1,
+      1
+    ]
+  },
+  {
+    name: "Match",
+    songLink: "https://www.youtube.com/watch?v=zqv3YesdtRU",
+    scores: [
+      5,
+      2,
+      4,
+      4,
+      5,
+      3,
+      2,
+      5,
+      4,
+      2
+    ]
+  },
+  {
+    name: "Ahmed",
+    songLink: "https://www.youtube.com/watch?v=zqv3YesdtRU",
+    scores: [
+      5,
+      1,
+      1,
+      4,
+      5,
+      1,
+      3,
+      5,
+      4,
+      2
+    ]
+  },
+  
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -29,8 +29,47 @@ module.exports = function(app) {
 
   app.post("/api/survey", function(req, res) {
       var newUser = req.body;
-      friendData.push(newUser);
-      res.json(true);
+      for (var i = 0; i< newUser.scores.length; i++)
+      {
+        newUser.scores[i] = parseInt(newUser.scores[i]);
+      }
+
+      friendData.push(newUser);    
+
+      var newUserScore = newUser.scores;
+
+      var diffSumArr = [];
+
+      for (var i = 0; i < friendData.length - 1; i++)
+      {
+        var diffSum = 0;
+        var previousUserScore = friendData[i].scores;
+        console.log(previousUserScore);
+
+        for (var j = 0; j < previousUserScore.length; j++)
+        {
+          var diff = previousUserScore[j] - newUserScore[j];
+          if (diff < 0)
+          {
+            diff = 0 - diff;
+          }
+          diffSum += diff;
+        }
+        diffSumArr.push(diffSum);
+      }
+
+      var lowDiff = diffSumArr[0];
+      var lowDiffIndex;
+      for (var i = 1; i < diffSumArr.length; i++)
+      {
+        if (diffSumArr[i] < lowDiff)
+        {
+          lowDiff = diffSumArr[i];
+          lowDiffIndex = i;
+        }
+      }
+
+      res.json({matchedUser: friendData[lowDiffIndex]});
   });
 
 };
